e2424d8c7d762b051331de4dcfb86e542392af52
Moksleivis <Moksleivis@kitm.local>
2023-03-10 11:38:42 +0200

    first commit

---
+++ firebase.js
@@ -0,0 +1,14 @@
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8rhxlQejAd8d8Zdc9Qt18vWHnIlnrfno",
+  authDomain: "ads-prc.firebaseapp.com",
+  databaseURL: "https://ads-prc-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ads-prc",
+  storageBucket: "ads-prc.appspot.com",
+  messagingSenderId: "254633494169",
+  appId: "1:254633494169:web:e3c67c9604e5cda0541a08"
+};
+
+export {firebaseConfig}
+
